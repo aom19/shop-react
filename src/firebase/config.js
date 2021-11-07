5aa863f2edb0979f3f687ae8037014d6e5ea7b5b
aom19 <39848284+aom19@users.noreply.github.com>
2021-11-07 12:38:24 +0200

    Shop

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQy7MvPIpNb2unDvnRdG-RsFdcz8SS2zU",
+  authDomain: "shop-8bf8a.firebaseapp.com",
+  projectId: "shop-8bf8a",
+  storageBucket: "shop-8bf8a.appspot.com",
+  messagingSenderId: "264954917507",
+  appId: "1:264954917507:web:c364be3654e5edafd921b6",
+  measurementId: "G-3D6Y470N08",
+};
